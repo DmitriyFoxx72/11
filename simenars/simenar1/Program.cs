0f706ea92e312824fa3ed43ef46988d3a500b9dc
dmitriy <foxx72@list.ru>
2022-11-24 17:44:57 +0300

    Сименар1

--- simenars/simenar1/Program.cs
+++ simenars/simenar1/Program.cs
@@ -1,2 +1,100 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Console.WriteLine( "seminar1");
+Console.WriteLine( "Input  A");
+int namberA = Convert.ToInt32(Console.ReadLine( ));
+Console.WriteLine( "Input  B");
+int namberB = Convert.ToInt32(Console.ReadLine( ));
+
+if( namberA > namberB)
+{
+        Console.WriteLine("А больше В");
+ }       
+else
+{
+             Console.WriteLine("В больше А");
+}
+/*
+
+
+
+/*
+Console.WriteLine( "seminar1");
+Console.WriteLine( "Input A ");
+int a = Convert.ToInt32(Console.ReadLine( ));
+Console.WriteLine( "Input B");
+int b = Convert.ToInt32(Console.ReadLine( ));
+Console.WriteLine( "Input C ");
+int c = Convert.ToInt32(Console.ReadLine( ));
+
+int max = a;
+
+if(a > max) max = a;
+if(b > max) max = b;
+if(c > max) max = c;
+
+Console.Write("Максимальное чило = ");
+Console.WriteLine(max);*/
+
+
+
+
+/*Console.WriteLine( "seminar1");
+Console.WriteLine( "Input a ");
+int a = Convert.ToInt32(Console.ReadLine( ));
+
+if(a % 2 == 0) 
+{
+        Console.WriteLine($" {a} является чётным");
+ }
+ 
+        else
+ {
+            Console.WriteLine($" {a} не является чётным");
+ }    
+*/
+
+/*
+Console.WriteLine( "seminar1");
+Console.WriteLine( "Input N ");
+int N = Convert.ToInt32(Console.ReadLine( ));
+int n =2;
+        while(n <= N)
+{    
+        Console.WriteLine(n);
+        n = n+2;
+} 
+*/
+
+// Программа, кторая выводит случайное число из отрезка [10,99] и показывает наибольшую цифру числа
+
+/*
+int FindBiggestDigit (int num)
+{
+    int ed = num  % 10;
+    int dec = num / 10;
+    if (ed > dec)
+    {
+        return ed;
+    }
+    else
+    {
+       return dec; 
+    }
+}
+int rand = new Random().Next(10,100);
+int result = FindBiggestDigit(rand);
+ Console.WriteLine($"Your number -> {rand}");  
+ Console.WriteLine($"The biggest digit  -> {result}");   
+*/
+
+
+
+
+
+
+
+
+
+
+
+
